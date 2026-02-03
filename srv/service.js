bf0d2865b9792682687141cce341fee27e3f1ff8
Ngoc Le <10422054@student.vgu.edu.vn>
2026-02-03 09:05:28 +0000

    new update

--- srv/service.js
+++ srv/service.js
@@ -1,10 +1,10 @@
-// const cds = require('@sap/cds');
+const cds = require('@sap/cds');
 
-// module.exports = async function () {
-//   const { A_BusinessPartner } = this.entities;
+module.exports = async function () {
+  const { A_BusinessPartner } = this.entities;
 
-//   this.on('READ', A_BusinessPartner, async (req) => {
-//     const bpSrv = await cds.connect.to('BusinessPartnerA2X');
-//     return bpSrv.run(req.query);
-//   });
-// };
+  this.on('READ', A_BusinessPartner, async (req) => {
+    const bpSrv = await cds.connect.to('BusinessPartnerA2X');
+    return bpSrv.run(req.query);
+  });
+};
